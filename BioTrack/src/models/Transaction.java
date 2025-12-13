759029dd59862ba16ae0aa01a3701e8f3b04eb8a
shieanne <shieannesulpico@gmail.com>
2025-12-14 03:20:33 +0800

    meow

--- BioTrack/src/models/Transaction.java
+++ BioTrack/src/models/Transaction.java
@@ -1,3 +1,24 @@
 public class Transaction {
-    
+
+    @SuppressWarnings("FieldMayBeFinal")
+    private String equipmentId;
+    @SuppressWarnings("FieldMayBeFinal")
+    private String equipmentName;
+    private final String action;      // Borrow / Return
+    private final String borrower;    // For borrowing
+    private final String dateTime;    // Timestamp
+
+    public Transaction(String equipmentId, String equipmentName, String action, String borrower, String dateTime) {
+        this.equipmentId = equipmentId;
+        this.equipmentName = equipmentName;
+        this.action = action;
+        this.borrower = borrower;
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return equipmentId + "|" + equipmentName + "|" + action + "|" + borrower + "|" + dateTime;
+    }
 }
+
